901d7a7dfbfdbf32c66aa0bdbacdbaa6d9a93e16
Nacho Molano <imolanor578@gmail.com>
2022-02-03 18:09:14 -0500

    Juego Del Ahorcado

---
+++ archivos.py
@@ -0,0 +1,23 @@
+def read():
+    numbers = []
+    with open("./archivos/numbers.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            numbers.append(int(line))
+        print(numbers)
+
+
+
+def write():
+    names = ["Facundo", "Nacho", "Moga", "Pepe", "Martín"]
+    with open("./archivos/names.txt", "a", encoding="utf-8") as f:
+        for name in names:
+            f.write(name)
+            f.write("\n")
+
+
+def run():
+    write()
+
+
+if __name__ == "__main__":
+    run()
